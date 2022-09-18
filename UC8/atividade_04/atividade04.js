4c401c97260c3eb33e7f785bc50c3475798eafdb
alex sander <alexsanderrosario2010@hotmail.com>
2022-09-18 10:24:49 -0300

    atividade 4 UC8

---
+++ UC8/atividade_04/atividade04.js
@@ -0,0 +1,29 @@
+let cadastrodeeventos = ["Ana","paulo","Maria"]
+
+cadastrodeeventos.push("Beatris")
+cadastrodeeventos.push("João")
+cadastrodeeventos.push("Sandra")
+    console.log(cadastrodeeventos)
+
+if (cadastrodeeventos.length <100){
+    console.log("É possivel cadastra pessoas mais evento!!!")
+}
+else {
+    console.log("Evento lotado não é possívelfazer novos cadastros")
+}
+
+let idade = 18 
+if(idade >= 18){
+    console.log("A idade está correta para o cadastro")
+}
+else{
+    console.log("Não é permitido cadastra menor que 18 anos")
+}
+
+
+
+
+
+
+
+
