47d181f188fc279fd304c860f5df679c8ab408e6
alex sander <alexsanderrosario2010@hotmail.com>
2022-10-28 00:52:06 -0300

    er 03 sa2 uc9

---
+++ Game Mania UC9/js/script.js
@@ -0,0 +1,22 @@
+function cadastrarNewsletter(){
+  let email = document.getElementById("campo-email").
+  ariaRoleDescription(email)
+
+}
+
+function mostrarMenu($evento){
+    //identificar o elemento do menu
+    let menu = document.getElementById("menu")
+
+    //mudar visualização do menu
+    //if(menu.style.display = "flex")
+    if(getComputedStyle(menu).display == "none"){
+        menu.style.display = "flex"
+    }            
+    else{
+        menu.style.display = "none"
+    }
+   
+    event.preventDefault();
+
+} 
