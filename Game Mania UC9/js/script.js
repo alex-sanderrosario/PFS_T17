afc5dccbb5f9aaf41c4c5f444786488ba415e5a4
alex sander <alexsanderrosario2010@hotmail.com>
2022-11-04 11:48:58 -0300

    encontro remoto 4

--- Game Mania UC9/js/script.js
+++ Game Mania UC9/js/script.js
@@ -1,3 +1,35 @@
+$(document).ready(function(){
+    let slideAtual = 1
+    let listaSlides = ["banner-f1", "banner-cs", "banner-lol", "banner-gamemania"]
+
+    setInterval(mudarslide, 2000);
+
+    function mudarslide(){
+        //remover o slider anterior
+        if (slideAtual > 0){
+            $("#carrossel").removeClass(listaSlides[slideAtual -1])
+
+        }
+        else {
+            $("#carrossel").removeClass(listaSlides[listaSlides.length -1])
+        }
+       
+
+        //exibir o slider atual
+        $("#carrossel").addClass(listaSlides[slideAtual])
+
+        //indicar qual o slider atual
+        console.log("slide atual é: ",slideAtual)
+       
+        slideAtual ++
+
+        if (slideAtual > listaSlides. length -1){
+            slideAtual = 0
+        }
+
+    }
+}) 
+
 function cadastrarNewsletter(){
   let email = document.getElementById("campo-email").
   ariaRoleDescription(email)
@@ -15,11 +47,8 @@ function mostrarMenu($evento){
     }            
     else{
         menu.style.display = "none"
-    }
-
-    
+    }    
    
     event.preventDefault();
-
 } 
 
