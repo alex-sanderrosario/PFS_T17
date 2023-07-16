a2c309c022505a413c3106297840c13ac23a82c1
alex-sanderrosario <alexsanderrosario2010@hotmail.com>
2023-07-16 15:54:11 -0300

    04e_atividade 5

---
+++ UC15/04d_atividade_4/Projeto_teste/TestXUnit/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Projeto_teste;
+
+namespace TestXUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            //Arrangem _ preparação 
+            double pNum = 1;
+            double sNum = 1;
+            double rNum = 2;
+
+            //Act
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+
+            //Assert
+            Assert.Equal(rNum, resultado);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 1, 2)]
+
+        public void SomarDoisNumerosLista(double pNum, double sNum, double rNum)
+        {
+
+
+            //Act
+            var resultado = Operacoes.Somar(pNum, sNum);
+
+
+            //Assert
+            Assert.Equal(rNum, resultado);
+        }
+    }
+}
